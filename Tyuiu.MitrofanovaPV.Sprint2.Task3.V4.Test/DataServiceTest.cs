0dae752b357aef49afcaed83239872f1e1525300
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-10-15 00:18:01 +0500

    Task3

---
+++ Tyuiu.MitrofanovaPV.Sprint2.Task3.V4.Test/DataServiceTest.cs
@@ -0,0 +1,42 @@
+﻿
+using Tyuiu.MitrofanovaPV.Sprint2.Task3.V4.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint2.Task3.V4.Test
+{
+    [TestClass]
+    public class Test1
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 11;
+            Assert.AreEqual(res, wait);
+        }
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.083;
+            Assert.AreEqual(res, wait);
+        }
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -1;
+            double res = ds.Calculate(x);
+            double wait = -0.5;
+            Assert.AreEqual(res, wait);
+        }
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -10;
+            double res = ds.Calculate(x);
+            double wait = -109.9;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
