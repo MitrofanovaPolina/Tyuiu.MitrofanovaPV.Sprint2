1c1ee78735072c0e77291a68ed7fab741148e5f0
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-10-15 03:30:06 +0500

    Task6

---
+++ Tyuiu.MitrofanovaPV.Sprint2.Task6.V1.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint2.Task6.V1.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint2.Task6.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(28, ds.FindMonthDaysCount(2));
+        }
+    }
+}
---
+++ Tyuiu.MitrofanovaPV.Sprint2.Task6.V1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint2.Task6.V1.Lib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите номер месяца:");
+        int value = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Количество дней =" + ds.FindMonthDaysCount(value));
+        Console.ReadKey();
+    }
+}
