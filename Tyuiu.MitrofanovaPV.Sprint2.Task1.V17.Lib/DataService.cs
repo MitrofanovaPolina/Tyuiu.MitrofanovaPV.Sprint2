b43a44f7ea5f88bd48a31f6c016c8bc5fe6a4b9d
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-10-14 21:27:13 +0500

    Task1

---
+++ Tyuiu.MitrofanovaPV.Sprint2.Task1.V17.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.MitrofanovaPV.Sprint2.Task1.V17.Lib
+{
+    public class DataService : ISprint2Task1V17
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+            res[0] = (a>b) | (d<c);
+            res[1] = (a<=b) & (d>=c);
+            res[2] = (a != b) || (d<c);
+            res[3] = (b<c) && (d>a);
+            res[4] = !(res[1]);
+            res[5] = (d>b) ^ (c != a);
+            return res;
+        }
+    }
+}
